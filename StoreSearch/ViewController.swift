c2d367a6bd7fce133758063bc101cdc39dd587bb
Patrick Gassler <pgassler424@gmail.com>
2022-02-20 16:37:24 -0500

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Samantha Smith on 2/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
