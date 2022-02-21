45c46ac668d3ed912f1e07bc6d6c123fadfc5d11
Patrick Gassler <pgassler424@gmail.com>
2022-02-20 21:49:05 -0500

    Post Chap 32

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Samantha Smith on 2/20/22.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
